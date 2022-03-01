254d61fe4a857ae93ebc47875403cc4e3e96d8bf
0xgrimm <97023949+0xgrimm@users.noreply.github.com>
2022-03-01 13:49:54 +0100

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -1,5 +1,4 @@
-
-;; GENERAL UI CONFIGURATION 
+;; UI CONFIGURATION 
 (setq inhibit-startup-message t  ; Don't show the splash screen
       visible-bell t)            ; Flash when the bell rings
 
@@ -7,10 +6,13 @@
 (tool-bar-mode -1)    ; Disable tool bar
 (scroll-bar-mode -1)  ; Disable scroll bar
 
-(global-display-line-numbers-mode 1) ; Display line numbers in every buffer
+(setq use-dialog-box nil) ; Don't pop up UI dialogs when prompting
+
+…(setq custom-file (locate-user-emacs-file "custom-vars.el")) ; Change the file where emacs will write variables
+(load custom-file 'noerror 'nomessage)                       ; Load the file with the variables without showing error messages
+
+(global-auto-revert-mode 1)                  ; Revert buffers when the underlying file has changed
+(setq global-auto-revert-non-file-buffers t) ; Revert dired and other buffers
 
 ;; THEME
 (load-theme 'modus-vivendi) ; Load modus vivendi dark theme
-
-
-
